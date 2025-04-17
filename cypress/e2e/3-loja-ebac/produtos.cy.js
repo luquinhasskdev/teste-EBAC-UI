edf75c612e363c7ae8fd6304a3ebc8fa2e486d97
Lucas <lukaaks1@gmail.com>
2025-04-17 17:09:01 -0300

    Teste com massa de dados em lista

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -22,10 +22,25 @@ describe('Funcionalidade: Produtos', () => {
     });
 
     it('Deve visitar a página do produto', () => {
-        
+        produtosPage.visitarProduto('Zeppelin-Yoga-Pant')
+        cy.get('.product_title').should('contain', 'Zeppelin Yoga Pant')
     });
 
     it('Deve adicionar produto ao carrinho', () => {
+        produtosPage.buscarProduto('Abominable Hoodie')
+        produtosPage.addProdutoCarrinho('M', 'Blue', 5)
+        cy.get('.stock').should('contain', 'Fora de estoque')
+    });
+
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+        produtosPage.buscarProduto(dados[1].nomeProduto)
+        produtosPage.addProdutoCarrinho(
+            dados[1].tamanho, 
+            dados[1].cor, 
+            dados[1].quantidade)
+        cy.get('div.woocommerce-message').should('contain', dados[1].nomeProduto)
+        })
         
     });
 });
